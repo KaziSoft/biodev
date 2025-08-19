5a7ba5247093af6ba56b2d942eb94b9ccc6d5f7d
taeef <kazitaeef@gmail.com>
2025-08-19 13:57:10 +0600

    Change some mejor configurations

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,17 @@
 import Slider from "./components/Carousel";
+import ClientLogoSlider from "./components/ClientLogoSlider";
+import ViewProjectsPage from "./components/ProjectGallery";
 import PromotionalBanner from "./components/PromotionalBanner";
+import ViewBlogs from "./components/ViewBlogs";
 
 export default function Home() {
   return (
     <>
       <Slider />
+      <ViewProjectsPage/>
       <PromotionalBanner />
+      <ViewBlogs/>
+      <ClientLogoSlider/>
     </>
   );
 }
--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    domains: [ 'images.ctfassets.net', 'res.cloudinary.com'],
+    unoptimized: true,
+  },
+  
+  eslint:{
+    ignoreDuringBuilds:true,
+  },
 };
 
 export default nextConfig;
